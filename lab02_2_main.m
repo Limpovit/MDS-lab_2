1c9f00f9c768a4c9c62d31ad3280045bdd059e61
Limpovit <32154871+Limpovit@users.noreply.github.com>
2017-10-05 19:12:05 +0300

    3

--- lab02_2_main.m
+++ lab02_2_main.m
@@ -86,6 +86,72 @@ plot(t, ecgf5, 'k-'), title(''), xlim([1 1.25]), hold off,
 xlabel('t, мс')
 ylabel('A, мкВ')
 
+%=== Завдання #3.1 ===
+% Дослідження властивостей простого режекторного фільтру
+br = [1 0.618 1];   % коефіцієнти різницевого рівняння 
+ar = 1;
+n =  512;	% кількість точок, що розраховуються
+fs = 200;
+figure()
+[h, f] = freqz(br, ar, n, fs);
+mag = abs(h); 
+phase = angle(h)*180/pi;
+subplot(3, 1, 1); plot(f, mag), grid on, title('АЧХ'),
+xlabel('Frequency'), ylabel('Magnitude'),
+subplot(3, 1, 2), plot(f, unwrap(phase)), grid on, title('ФЧХ'),
+xlabel('Frequency'), ylabel('Phase'),
+% Обчислення нулів фільтру
+x = roots(br);
+% Карта нулів та полюсів фільтру
+subplot(3, 1, 3), zplane(x)
+
+%=== Завдання #3.2 ===
+% Фільтрація сигналу ЕКГ (файл ecg2х60.dat) режекторним фільтром
+fs = 250;
+ecg2x60 = load('ecg2x60.dat');
+ecg = ecg2x60;	% сигнал ЕКГ
+ecg = detrend(ecg);
+ecgf = filter(br, ar, ecg);
+t = (0:length(ecg) - 1)/fs;	
+figure()
+plot(t, ecg), title('Початковий та відфільтрований сигнали'), hold on
+plot(t, ecgf, 'k-'), xlim([0 1]),  hold off,
+xlabel('t, мс')
+ylabel('A, мкВ')
+
+%=== Завдання #4.1 ===
+% Дослідження властивостей диференціаторів
+% N = 1
+fs = 1000;
+bd1 = [1 -1]		% коефіцієнти різницевого рівняння 
+ad1 = 1;
+n = 512	;	 % кількість точок, що розраховуються
+figure()
+[h,w] = freqz(bd1,ad1,n);
+mag = abs(h); 
+phase = angle(h)*180/pi;
+subplot(3, 1, 1); plot(w/(2*pi)*fs,mag), grid on, title('АЧХ'),
+xlabel('Frequency'), ylabel('Magnitude'),
+subplot(3, 1, 2), plot(w/(2*pi) *fs,unwrap(phase)), grid on, title('ФЧХ'),
+xlabel('Frequency'), ylabel('Phase'),
+
+% N = 2
+bd2 = [1 -1]/2;   	% коефіцієнти різницевого рівняння 
+ad2 = 1;
+n = 512 ;	  % кількість точок, що розраховуються
+figure()
+[h,w] = freqz(bd2,ad2,n);
+mag = abs(h); 
+phase = angle(h)*180/pi;
+subplot(3, 1, 1); plot(w/(2*pi)*fs,mag), grid on, title('АЧХ'),
+xlabel('Frequency'), ylabel('Magnitude'),
+subplot(3, 1, 2), plot(w/(2*pi) *fs,unwrap(phase)), grid on, title('ФЧХ'),
+xlabel('Frequency'), ylabel('Phase'),
+
+
+% Побудова передавальних функцій
+H1 = filt ()
+H2 = filt ()
 
 
 
